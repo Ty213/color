accf034c93d1d2a4d2978fded73197af8a6d1dfc
Ty Thompson <tylermoko@gmail.com>
2018-05-14 16:03:32 -0400

    adding grid and keypress events

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-var canvas = document.querySelector(".canvas");
+//grabbing elements for document
+var canvas = document.querySelector(".game");
+var grid = document.querySelector(".grid");
 var colorPicker = document.querySelector(".color-picker");
-canvas.width  = 400;
-canvas.height = 400;
 
 //size of player and grid spacing
 const UNIT = 25;
-//draws the grid on the canvas
+const SIZE = 400;
+//setting the size for both canvases
+grid.width = SIZE;
+grid.height = SIZE;
+canvas.width  = SIZE;
+canvas.height = SIZE;
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+  });
+
+//draws the grid on the grid-canvas
 drawBoard();
 
+
 class Player {
-    constructor(x, y, color="#777") {
+    constructor(x=0, y=0, color="#777") {
       this.x = x;
       this.y = y;
       this.color = color;
@@ -30,23 +43,25 @@ class Player {
     }
   }
 
-var player = new Player(0,0);
+var player = new Player();
 
+//when a new color is selected
 function getColor() {
     player.setColor();
     player.drawPlayer();
 }
 
+//draws the grid on the grid canvas
 function drawBoard() {
-    var ctx = canvas.getContext("2d");
+    var ctx = grid.getContext("2d");
     
-    for(var i = 0; i <= canvas.width; i += UNIT) {
+    for(var i = 0; i <= grid.width; i += UNIT) {
         ctx.moveTo(i, 0);
-        ctx.lineTo(i, canvas.height);
+        ctx.lineTo(i, grid.height);
     }
-    for(var i = 0; i <= canvas.height; i += UNIT) {
+    for(var i = 0; i <= grid.height; i += UNIT) {
         ctx.moveTo(0, i);
-        ctx.lineTo(canvas.width, i);
+        ctx.lineTo(grid.width, i);
 
     }
   
